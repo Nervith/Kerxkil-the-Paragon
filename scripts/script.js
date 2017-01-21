8326cc59f2c723938ab8c97548bacf20351f36ec
Harsh Patel <harshnp02@gmail.com>
2017-01-21 18:10:30 -0500

    Simple Beginnings

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+var game = function {
+var gmeover = false;
+if(gameover === true) {
+  alert("Game Over");
+}
+alert("This si the tale of Krxkil, the last Paragon");
+alert("Lord of Gharrix");
+alert("Immortal");
+alert("To serve is his purpose");
